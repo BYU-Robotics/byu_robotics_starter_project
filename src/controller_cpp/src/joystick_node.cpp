8cb0ff8fccf147850ee7d75aff579def11085f69
nalyd008 <dylancolehale@gmail.com>
2026-05-23 23:44:36 -0600

    Created basic controller  node to understand /joy message and turn them into /cmd_vel messages for robot movement

--- src/controller_cpp/src/joystick_node.cpp
+++ src/controller_cpp/src/joystick_node.cpp
@@ -1,10 +1,37 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/joy.hpp"
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+#include <iostream>
 
-  printf("hello world controller_cpp package\n");
+class ControllerNode : public rclcpp::Node{
+  private:
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscription_; 
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_publisher_;
+    void joy_to_twist(const sensor_msgs::msg::Joy::SharedPtr msg){
+      auto twist_publish = geometry_msgs::msg::Twist();
+      if(msg->axes.size() >= 4){ 
+        twist_publish.linear.x = msg->axes.at(1);
+        twist_publish.angular.z = msg->axes.at(3);
+        RCLCPP_INFO(this->get_logger(), "Linear: %.2f Angular Z: %.2f", msg->axes.at(1), msg->axes.at(3));
+      }
+      else{
+        twist_publish.linear.x = 0.0;
+        twist_publish.angular.z = 0.0;
+        RCLCPP_INFO(this->get_logger(), "Controller Error");
+      }
+      twist_publisher_->publish(twist_publish);
+    }
+  public:
+    ControllerNode():Node("ControllerNode"){
+      joy_subscription_ = this->create_subscription<sensor_msgs::msg::Joy>("joy",10,std::bind(&ControllerNode::joy_to_twist,this,std::placeholders::_1)); 
+      twist_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
+    }
+};
+
+int main(int argc, char** argv){
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ControllerNode>());
+  rclcpp::shutdown();
   return 0;
 }
