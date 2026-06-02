37ed4c920583045d85ff754b689d00cbb2bf5697
nalyd008 <dylancolehale@gmail.com>
2026-06-01 22:15:02 -0600

    Added comments to launch file

--- src/controller_cpp/launch/teleop_launch.py
+++ src/controller_cpp/launch/teleop_launch.py
@@ -1,17 +1,19 @@
+# This launch file starts the joystick driver and the teleop translator node for controlling a robot using a joystick.
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-
-
     return LaunchDescription([
+        # Declare a launch argument for the joystick device
         Node(
             package='joy',
             executable='joy_node',
             name='controller_driver',
         ),
+
+        # Declare a launch argument for the teleop translator node
         Node(
             package='controller_cpp',
             executable='teleop',
